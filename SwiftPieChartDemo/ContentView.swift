2cd3ce124db11b4ca36b6d8a5b5a49adae43edbb
Nazar Ilamanov <nazar.ilamanov@gmail.com>
2021-04-24 23:42:43 -0400

    Simple demo

--- SwiftPieChartDemo/ContentView.swift
+++ SwiftPieChartDemo/ContentView.swift
@@ -6,11 +6,21 @@
 //
 
 import SwiftUI
+import SwiftPieChart
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+                .frame(height: 150.0)
+            Text("Spent this month")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+            PieChartView(values: [1300, 500, 300], names: ["Rent", "Transport", "Education"], formatter: {value in String(format: "$%.2f", value)})
+                .padding(7.0)
+        }
+        .background(Color(red: 21 / 255, green: 24 / 255, blue: 30 / 255, opacity: 1.0))
+        .edgesIgnoringSafeArea([.top, .bottom])
     }
 }
 
