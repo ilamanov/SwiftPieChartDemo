5e58b6a95671fed0fb9863bc9b32c0f998d01c22
Nazar Ilamanov <nazar.ilamanov@gmail.com>
2021-04-24 22:59:01 -0400

    Initial Commit

---
+++ SwiftPieChartDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftPieChartDemo
+//
+//  Created by Nazar Ilamanov on 4/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftPieChartDemo/SwiftPieChartDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPieChartDemoApp.swift
+//  SwiftPieChartDemo
+//
+//  Created by Nazar Ilamanov on 4/24/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPieChartDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
